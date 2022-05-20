9179bdffe28a145d54ce0bddfff7b341a21fbb29
Rodrigo Trevizan <rlt@tuta.io>
2022-05-20 00:28:00 -0400

    fix typo

--- src/App.test.js
+++ src/App.test.js
@@ -6,11 +6,11 @@ test('button has correct inicial color', () => {
  
     const colorButton = screen.getByRole('button', { name: 'Gray button' });
 
-    expect(colorButton).toHaveStyle("background-color: gray"); // suceeds
-    expect(colorButton).toHaveStyle( {backgroundColor: 'gray', display: 'inline-block' }); // suceeds
-    expect(colorButton).toHaveStyle( {backgroundColor: 'gray' }); // suceeds
+    expect(colorButton).toHaveStyle("background-color: gray"); // succeeds
+    expect(colorButton).toHaveStyle( {backgroundColor: 'gray', display: 'inline-block' }); // succeeds
+    expect(colorButton).toHaveStyle( {backgroundColor: 'gray' }); // succeeds
     expect(colorButton).toHaveStyle( {backgroundColor: 'fay' }); // suceeds but should fail
-    expect(colorButton).toHaveStyle( {backgroundColor: 'dfhdhsdh' }); // suceeds but should fail
-    expect(colorButton).not.toHaveStyle( {backgroundColor: 'red' }); // suceeds
+    expect(colorButton).toHaveStyle( {backgroundColor: 'dfhdhsdh' }); // succeeds but should fail
+    expect(colorButton).not.toHaveStyle( {backgroundColor: 'red' }); // succeeds
     expect(colorButton).toHaveStyle( {backgroundColor: 'red' }); // failed
 })
